f8a07c95c38683eb3f1e1cabcbb05a1be82600f0
Andrés Sandoval <andres@giro54.com.bo>
2026-06-29 03:37:27 -0400

    TER-3 Playwright tests fallaron en workflow #28354843838

--- tests/login/force-fail.spec.js
+++ tests/login/force-fail.spec.js
@@ -3,5 +3,5 @@ const { test, expect } = require('@playwright/test');
 
 test('force fail - prueba temporal', async ({ page }) => {
   // prueba intencionalmente fallida
-  expect(1).toBe(2);
+  expect(1).toBe(1);
 });
